49927c3068523b48f44a8e303d45f5113fc84c6a
Haytham ElFadeel <HFadeel@users.noreply.github.com>
2021-08-16 23:59:18 -0700

    v2

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20210816.2331';
+const cacheName = 'chirpy-20210816.2357';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
